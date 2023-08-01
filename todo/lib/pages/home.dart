c7c59362c3760ae7a61f44950fec22c4dcd96a39
peacebefore <peacebefore@gmail.com>
2023-07-31 18:09:58 -0700

    Built out 'add todo' functionality

--- todo/lib/pages/home.dart
+++ todo/lib/pages/home.dart
@@ -1,10 +1,10 @@
-
-
 import 'package:flutter/material.dart';
-
+import 'package:todo/pages/add.dart';
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
+  const MyHomePage({
+    super.key,
+  });
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
@@ -14,16 +14,17 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    
     return Scaffold(
       appBar: AppBar(
-        title: Text("Todo App"),
+        title: const Text("Todo App"),
       ),
-      body: Center(
-        
-      ),
+      body: const Center(),
       floatingActionButton: FloatingActionButton(
-        onPressed: (){},
+        onPressed: () {
+          Navigator.of(context).push(
+            MaterialPageRoute(builder: (context) => const AddTodo()),
+          );
+        },
         tooltip: 'Increment',
         child: const Icon(Icons.add),
       ), 
